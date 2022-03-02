48b2a5dc99b3b28723770e036e9e38d92d3ec765
40923127-1 <91860210+40923127-1@users.noreply.github.com>
2022-03-02 09:30:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cad2021_hw
+
+https://40923127-1.github.io/cad2021_hw/content/index.html
---
+++ downloads/20211107.js
@@ -0,0 +1,8 @@
+﻿var winkVideoData = {
+  dataVersion: 1,
+  frameRate: 7,
+  buttonFrameLength: 3,
+  buttonFrameOffset: 1,
+  frameStops: {
+  },
+};
